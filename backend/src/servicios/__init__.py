fbc6c03c68b43d550903671e96519dc156fa8f28
joselinki25 <josepalof@gmail.com>
2026-07-02 21:49:16 -0500

    fix: conflicto init.py de backend/src/rutas

--- backend/src/servicios/__init__.py
+++ backend/src/servicios/__init__.py
@@ -4,19 +4,11 @@ from src.servicios.solicitud_service import (
     SolicitudService,
     get_solicitud_service,
 )
-<<<<<<< HEAD
-from src.servicios.voto_service import VotoService, get_voto_service
-=======
->>>>>>> b465b534a671ea539fa2e44c93345b99b8c45799
 from src.servicios.auth_service import AuthService, get_auth_service
 
 __all__ = [
     "SolicitudService",
     "get_solicitud_service",
-<<<<<<< HEAD
-    "get_voto_service",
-=======
->>>>>>> b465b534a671ea539fa2e44c93345b99b8c45799
     "AuthService",
     "get_auth_service",
 ]
